c68ca8a37874c6d8dc948260e80aa2cdef8d0816
cj <subashcj007@gmail.com>
2020-04-23 16:26:14 +0530

    updated the app to v2

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,10 +2,10 @@ var express = require('express');
 app = express();
 
 
-
 var response;
 
 app.get('/', function (req, res) {
+
     response = 'This is version 2 of the app.' + '\n';
 
     //send the response to the client
@@ -13,3 +13,6 @@ app.get('/', function (req, res) {
 
 });
 
+app.listen(8080, function () {
+  console.log('Server listening on port 8080...');
+});
